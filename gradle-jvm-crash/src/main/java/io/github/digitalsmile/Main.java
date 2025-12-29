c1bc62c69315ac4a1cc15ec9e6a1e4a160dc4427
digitalsmile <digitalsmile.dig@gmail.com>
2025-12-29 16:55:21 +0300

    Added comments

--- gradle-jvm-crash/src/main/java/io/github/digitalsmile/Main.java
+++ gradle-jvm-crash/src/main/java/io/github/digitalsmile/Main.java
@@ -2,6 +2,9 @@ package io.github.digitalsmile;
 
 public class Main {
     static void main() {
+        /*
+         Works fine
+         */
         var indexNative = new IndexNative();
         var indexMemorySegment = indexNative.createIndex(0, 0);
         indexNative.disposeIndex(indexMemorySegment);
--- gradle-jvm-crash/src/test/java/TestJvmCrash.java
+++ gradle-jvm-crash/src/test/java/TestJvmCrash.java
@@ -8,6 +8,9 @@ public class TestJvmCrash {
 
     @Test
     public void simpleTest() {
+        /*
+         Always crashes, but test is marked green
+         */
         var indexNative = new IndexNative();
         var indexMemorySegment = indexNative.createIndex(0, 0);
         indexNative.disposeIndex(indexMemorySegment);
@@ -17,6 +20,9 @@ public class TestJvmCrash {
 
     @Test
     public void simpleTestWithConfinedArena() {
+        /*
+         Always crashes, but test is marked green
+         */
         var indexNative = new IndexNative();
         try (var offheap = Arena.ofConfined()) {
             var indexMemorySegment = indexNative.createIndexWithArena(offheap, 0, 0);
@@ -29,6 +35,9 @@ public class TestJvmCrash {
 
     @Test
     public void simpleTestGlibc() {
+        /*
+         Works fine
+         */
         var indexNative = new IndexNative();
         try (var offheap = Arena.ofConfined()) {
             var pointer = offheap.allocateFrom("Hello");
